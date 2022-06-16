b271c0d721914591a51636f7475d1d9f74c051cc
ANIRUDH SINGH <anirudh8400@gmail.com>
2022-06-16 15:59:58 +0530

    first commit

---
+++ DeligatesExample/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeligatesExample
+{
+    public delegate void MyOperations(int a, int b);
+    internal class Example
+    {
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine(a + b);
+
+        }
+        public void Subtraction(int a, int b)
+        {
+
+            Console.WriteLine(a - b);
+        }
+    }
+}
---
+++ DeligatesExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeligatesExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Example delegateExamples = new Example();
+                MyOperations myOperations = new MyOperations(delegateExamples.Addition);
+                myOperations += new MyOperations(delegateExamples.Subtraction);
+                myOperations(2, 9);
+                Console.WriteLine("============================");
+                List<Emp> emps = new List<Emp>
+            {
+                new Emp{Id =1, Name ="Anirudh",Exp=3,salary=10000},
+                new Emp{Id =2, Name ="Rahul",Exp=4,salary=25000},
+                new Emp{Id =3, Name ="Vickey",Exp=2,salary=320000},
+                new Emp{Id =4, Name ="Rajan",Exp=5,salary=420000},
+            };
+                IsPrmote isPrmote = new IsPrmote(Emp.Prmote);
+                //isPrmote()
+                Emp.Prmotion(emps, isPrmote);
+                Console.ReadKey();
+            }
+        }
+    }
+}
